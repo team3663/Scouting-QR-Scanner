efe047cafec2eb4a94235e07417b3589a066e1f5
Adriel Stammler <jstammler@msn.com>
2025-01-14 19:20:59 -0800

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,22 @@
 import javax.swing.*;
 
 public class Main extends JFrame {
+    private JTextField txtFileData;
+    private JPanel MainPanel;
+    private JButton btn_Save;
+
     public Main () {
+        setContentPane(MainPanel);
         setTitle("CPR 3663 - Scouting QR Scanner");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(600,900);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        btn_Save.addActionListener(e -> {
+            String data = txtFileData.getText();
+            JOptionPane.showMessageDialog(Main.this, "Clicked the button! " + data);
+        });
     }
 
     public static void main(String[] args) {
