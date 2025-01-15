d183b801862fb68ec86f2bd84603a6ec78325e77
Adriel Stammler <jstammler@msn.com>
2025-01-14 19:02:00 -0800

    initial

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public Main () {
+        setTitle("CPR 3663 - Scouting QR Scanner");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600,900);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    };
+
+    public static void main(String[] args) {
+        new Main();
+    }
+}
